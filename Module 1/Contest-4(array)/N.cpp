cea44526456e173f22ff21d04b31535a23441bbd
adnan-borshon <adnan.borshon@gmail.com>
2025-11-15 00:13:53 +0600

    N solved for array contest

--- Module 1/Contest-4(array)/N.cpp
+++ Module 1/Contest-4(array)/N.cpp
@@ -1,68 +1,46 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+
 int main(){
-    int array[100];
+    int array[10000];
     int N;
     cin>>N;
-    int i=0;
+
+    for(int i=0;i<N;i++) cin>>array[i];
+
+    int i=1;
     while(i<N){
-cin>>array[i++];
+        if((array[i]-array[i-1])==1 || (array[i]-array[i-1])==-1){
+            i++;
+            continue;
+        }
+
+    
+        int newtemp[10000];
+        int newtemp_size = 0;
+
+        if(array[i-1]<array[i]){
+            for(int x=array[i-1]+1;x<array[i];x++)
+                newtemp[newtemp_size++] = x;
+        } else {
+            for(int x=array[i-1]-1;x>array[i];x--)
+                newtemp[newtemp_size++]=x;
+        }
+
+        for(int k=N-1;k>=i;k--){
+            array[k+newtemp_size]=array[k];
+        }
+    
+        for(int k=0;k<newtemp_size;k++){
+            array[i+k]=newtemp[k];
+        }
+
+        N+=newtemp_size; 
+        i++;                
     }
 
-i=1;
-while(i<N && array[i]){
-    if(array[i]-array[i-1]!=1){
-        i++;
-        continue;
+    for(int k=0;k<N;k++){
+        cout<<array[k]<<" ";
     }
-if(array[i-1]<array[i] ){
-    int newtemp_size=array[i]-array[i-1]-1;
-    int newtemp[newtemp_size];
-    int j=0;
-    for(;j<newtemp_size;j++){
-        newtemp[j]=array[i-1]+1+j;
-    }
-
-    int from= i+1;
-    // int len=N-(i+1);
-    int len=newtemp_size;
-    int to=N-1;
-    rotate(array+from , array+from+len, array+to+len);
-   j=0;
-   int x=i+1;
-   for(;x<newtemp_size+i;x++){
-    array[x]=newtemp[j++];
-   }
-}
-else {
-    int newtemp_size=array[i-1]-array[i]-1;
-    int newtemp[newtemp_size];
-    int j=0;
-    for(;j<newtemp_size;j++){
-        newtemp[j]=array[i]-1-j;
-    }
-
-    int from= i+1;
-    // int len=N-(i+1);
-    int len=newtemp_size;
-    int to=N-1;
-    rotate(array+from , array+from+len, array+to+len);
-   j=0;
-   int x=i+1;
-   for(;x<newtemp_size+i;x++){
-    array[x]=newtemp[j++];
-   }
-}
-
-
-    i++;
-}
-
-
-i=0;
-while(array[i]){
-    cout<<array[i++]<<" ";
-}
-
 }
